b4a6b410d5f2289c1c0a29b32ca155901eb95564
Brian Palomar <104478722+9brian@users.noreply.github.com>
2022-10-09 11:15:57 -0700

    Add files via upload

---
+++ pltl_091021.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+//========Exercise 2=========
+/*
+double pi = 3.1415;
+double radius, area, circumference;
+
+int main() 
+{
+  
+  cout << "Find the area and circumference of any circle.\n";
+  cin >> circumference;
+  cout << "Input the radius of a circle: ";
+  cin >> radius;
+
+  circumference = 2 * pi * radius;
+  area = pi * radius * radius;
+
+  cout << "The area of the circle is: " << area << endl;
+  cout << "The circumference of the circle is: " << circumference << endl;		
+  
+  return 0;
+}
+*/
+
+//========Example 6=========
+/*
+int main()
+{
+  double length=0, width=0, area=0, perimeter =0;
+  cout << "Find the Area and Perimeter of a Rectangle: \n";
+  cout << "-------------------------------------------\n";
+  
+  cout << "Input the length of the rectangle: ";
+  cin >> length;
+
+  cout << "Input the width of the rectangle: ";
+  cin >> width;
+
+  area = length * width;
+ 
+  cout << "The area of the rectangle is: " << area << endl;
+  
+  perimeter = (length + width) * 2;
+
+  cout << "The perimeter of the rectangle is: " << perimeter << endl;
+
+  return 0;
+}
+*/
+
+//==========Exercise 7============
+/*
+int main()
+{
+  int num = 0, result = 1;
+  
+  cout << "Input your number: " << endl;
+  cin >> num;
+
+  result = num % 2;
+
+  if (result == 0) {
+    cout << num <<  " is an even number." << endl;
+  }
+  else {
+    cout << num << " is an odd number." << endl;
+  }
+  
+  // a = 12 % 2;
+  // cout << a << endl;
+
+  return 0;
+}
+*/
